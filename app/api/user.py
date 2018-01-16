3993058290ef826aa64a79efb724fe03a91f3dab
Hasan Hüseyin Pay <h.huseyinpay@gmail.com>
2018-01-16 11:26:43 +0300

    ADD /dailystudy/getday -/getinterval GET POST

--- app/api/user.py
+++ app/api/user.py
@@ -1,11 +1,13 @@
-from flask import request, jsonify
+# -*- coding: utf-8 -*-
+from flask import request, jsonify, json
 from flask_jwt_extended import jwt_required, get_jwt_identity, get_jwt_claims
 from datetime import datetime, timedelta
 from flask_sqlalchemy import get_debug_queries
 from sqlalchemy import extract
 from . import api
-from .errors import bad_request
-from ..models import DailyStudy
+from .errors import bad_request, internal_error
+from ..models import DailyStudy, DailyStudyCourse, Role
+from .. import db
 
 
 @api.after_app_request
@@ -18,58 +20,72 @@ def after_request(response):
     return response
 
 
-@api.route('/dailystudy/getbyday/<string:day>', methods=['GET'])
+@api.route('/dailystudy/getday/<string:day>', methods=['GET'])
 @jwt_required
 def dgetbydat(day):
     user_id = get_jwt_identity()
-    date = day.split("-")
     try:
+        date = day.split("-")
         studies = DailyStudy.query.filter_by(student_id=user_id).filter(
             extract('day', DailyStudy.date) == date[0], extract('month', DailyStudy.date) == date[1],
             extract('year', DailyStudy.date) == date[2]).all()
 
         print(studies)
-    except Exception as e:
-        print(e)
+    except Exception:
+        print(Exception)
         return bad_request("incorrect day parameter")
     return jsonify([study.to_json() for study in studies]), 200
 
 
-@api.route('/dailystudy/getbyinterval/<string:startday>/<string:endday>', methods=['POST'])
+@api.route('/dailystudy/getinterval/<string:startdate>/<string:enddate>', methods=['GET'])
 @jwt_required
-def dgetbyinterval(startday, endday):
+def dgetbyinterval(startdate, enddate):
     user_id = get_jwt_identity()
-    req = request.json.get('request')
-    if req == "getbyinterval":
-        try:
-            startday = request.json.get('startday')
-            endday = request.json.get('endday')
-            studies = DailyStudy.query.filter_by(student_id=user_id).filter(DailyStudy.date > startday).filter(
-                DailyStudy.date < endday).all()
-        except:
-            return bad_request("incorrect day parameter")
-        return jsonify([study.to_json() for study in studies]), 200
+    try:
+        startdate = datetime.strptime(startdate, "%d-%m-%Y")
+        enddate = datetime.strptime(enddate, "%d-%m-%Y")
 
-    return bad_request("body")
+        studies = DailyStudy.query.filter_by(student_id=user_id).filter(DailyStudy.date > startdate,
+                                                                        DailyStudy.date < enddate).all()
+    except Exception:
+        print(Exception)
+        return bad_request("incorrect day parameter")
+    return jsonify([study.to_json() for study in studies]), 200
 
 
-@api.route('/dailystudy/settoday', methods=['POST'])
+@api.route('/dailystudy', methods=['POST'])
 @jwt_required
-def dsettoday():
+def dailystudypost():
     user_id = get_jwt_identity()
-    req = request.json.get('request')
+    role = get_jwt_claims().get("role")
+    role_id = Role.query.filter_by(name=role).first().id
 
-    # time = datetime.utcnow() - timedelta(days=int(day))
-    if req == "settoday":
-        today = request.json.get('today')
+    courses = DailyStudyCourse.query.filter_by(role_id=role_id).all()
+    course_ids = [course.course_id for course in courses]
 
-        print(today)
-        return "0"
+    course = []
+    req = request.json.get('done')
+    for r in req:
+        if not r.get("course_id") in course_ids:
+            return bad_request("invalied course_id")
+        c = DailyStudy.from_json(user_id, r)
+        course.append(c)
 
-    return bad_request("body")
+    try:
+        db.session.add_all(course)
+        db.session.commit()
+    except Exception:
+        print(Exception)
+        return internal_error("db problem")
+    return jsonify("OK"), 200
 
 
 @api.route('/dailystudy', methods=['GET'])
 @jwt_required
 def dailystudyget():
-    return "0"
+    role = get_jwt_claims().get("role")
+    role_id = Role.query.filter_by(name=role).first().id
+
+    courses = DailyStudyCourse.query.filter_by(role_id=role_id).all()
+
+    return jsonify([course.course.name for course in courses]), 200
